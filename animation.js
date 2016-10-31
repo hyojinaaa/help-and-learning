ea96f8df861078dbe488b7970c5dcabee7953d7d
hyojinaaa <hyojinjin@hotmail.com>
2016-10-31 14:19:09 +1300

    Add jquery functions and all sections

--- animation.js
+++ animation.js
@@ -3,35 +3,85 @@ $(document).ready(function(){
 	$(window).scroll(function(){
 
 		var scrollPosition = $(window).scrollTop();
-		var help1one = $('#help1-1');
-		var help1two = $('#help1-2');
-		var help1three = $('#help1-3');
-		var help1four = $('#help1-4');
+		var help1 = $('#help1');
+		var help2 = $('#help2');
+		var help3 = $('#help3');
+		var help4 = $('#help4');
+		var help5 = $('#help5');
 
 		if( scrollPosition > 100 ) {
-			help1one.slideDown();
+			help1.slideDown();
 		} else {
-			help1one.hide();
+			help1.hide();
 		}
 
-		if( scrollPosition > 110 ) {
-			help1two.slideDown();
+		if( scrollPosition > 800 ) {
+			help2.slideDown();
 		} else {
-			help1two.hide();
+			help2.hide();
 		}
 
-		if( scrollPosition > 120 ) {
-			help1three.slideDown();
+		if( scrollPosition > 1500 ) {
+			help3.slideDown();
 		} else {
-			help1three.hide();
+			help3.hide();
 		}
 
-		if( scrollPosition > 130 ) {
-			help1four.slideDown();
+		if( scrollPosition > 2200 ) {
+			help4.slideDown();
 		} else {
-			help1four.hide();
+			help4.hide();
 		}
+
+
+		if( scrollPosition > 2900 ) {
+			help5.slideDown();
+		} else {
+			help5.hide();
+		}
+
 
 	});
 
+	
+	$('.play1').click(function(){
+		$('#video1').get(0).play();
+		$('.play1').hide();
+		$('.pause1').show();
+	});
+
+	$('.pause1').click(function(){
+		$('#video1').get(0).pause();
+		$('.play1').show();
+		$('.pause1').hide();
+	});
+
+	$('.play2').click(function(){
+		$('#video2').get(0).play();
+		$('.play2').hide();
+		$('.pause2').show();
+	});
+
+	$('.pause2').click(function(){
+		$('#video2').get(0).pause();
+		$('.play2').show();
+		$('.pause2').hide();
+	});
+
+	$('.play1').click(function(){
+		$('#video1').get(0).play();
+		$('.play1').hide();
+		$('.pause1').show();
+	});
+
+	$('.pause1').click(function(){
+		$('#video1').get(0).pause();
+		$('.play1').show();
+		$('.pause1').hide();
+	});
+
 });
+
+
+
+
